6a0be20443947cb91eeb0be4a7425114cf5e9e24
Lakshmisha Nanjappachar <lakshmisha@yahoo.com>
2020-09-28 16:57:42 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 provider "aws" {
   # MODIFY this line to look for 2.27.0 or greater
   version = ">= 2.27.0"
+  access_key = ""
+  secret_key = ""
+  region     = ""
+}
+
+resource "aws_instance" "web" {
+  # ...
 }
